51b088f9d35aaa6ca80129d4e2ee31934a33b9f1
Gwendal Paton <gwendalpaton@gmail.com>
2024-03-22 16:48:37 +0100

    clean code

--- events/guild_message/messageCreate.js
+++ events/guild_message/messageCreate.js
@@ -64,30 +64,30 @@ module.exports = {
                 .then(() => Logger.info(`Apagnan | ${message.author.username} a été crampté pour ${time} min`));
             client.users.send('217279235021209600', `Apagnan | ${message.channel.name} | ${message.author.username} a été crampté pour ${time} min`);
         }
-        if(message.content.toLowerCase().replace(regexPattern, "").replace().includes("flipreset"))
+        if(message.content.toLowerCase().replace(regexPattern, "").includes("flipreset"))
         {
             message.reply("C'est ta mère que je flip reset");
             Logger.info(`Flip reset ${message.author.username} | ${message.channel.name}`);
         }
-        if(message.content.toLowerCase().replace(regexPattern, "").replace().includes("démonte") && Math.random() < 0.30)
+        if(message.content.toLowerCase().replace(regexPattern, "").includes("démonte") && Math.random() < 0.30)
         {
             message.reply("C'est ta mère que je démonte");
             Logger.info(`Démonte ${message.author.username} | ${message.channel.name}`);
         }
-        if( (message.content.toLowerCase().replace(regexPattern, "").replace().includes("soulèv")
-        || message.content.toLowerCase().replace(regexPattern, "").replace().includes("soulev")) 
+        if( (message.content.toLowerCase().replace(regexPattern, "").includes("soulèv")
+        || message.content.toLowerCase().replace(regexPattern, "").includes("soulev")) 
         && Math.random() < 0.30)
         {
             message.reply("C'est ta mère que je soulève");
             Logger.info(`Soulève ${message.author.username} | ${message.channel.name}`);
         }
-        if(message.content.toLowerCase().replace(regexPattern, "").replace().includes("neodrift")
-        || message.content.toLowerCase().replace(regexPattern, "").replace().includes("néodrift"))
+        if(message.content.toLowerCase().replace(regexPattern, "").includes("neodrift")
+        || message.content.toLowerCase().replace(regexPattern, "").includes("néodrift"))
         {
             message.reply("Je néo drift dans ta daronne");
             Logger.info(`Neo ${message.author.username} | ${message.channel.name}`);
         }
-        if(message.content.toLowerCase().replace(regexPattern, "").replace().includes("quoi") && Math.random() < 0.10)
+        if(message.content.toLowerCase().replace(regexPattern, "").includes("quoi") && Math.random() < 0.10)
         {
             if(message.content.toLowerCase().includes("pourquoi")){
                 message.reply("Pour Feur").catch(error => console.log(error));
